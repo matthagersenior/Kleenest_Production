e5692d33740044406922e223e149570e10739961
matthagersenior <matthagersr@gmail.com>
2026-08-31 00:21:45 -0500

    feat(mobile): publish canonical notification overrides

--- packages/mobile-core/src/publicEntry.ts
+++ packages/mobile-core/src/publicEntry.ts
@@ -1,3 +1,4 @@
 export * from './index';
 export { hydratePublicContributors, listMobileLocationReviews, searchMobilePeople } from './publicContributors';
 export { listMobileActivity } from './privateActivity';
+export { listMobileNotifications, markMobileNotificationRead, markAllMobileNotificationsRead } from './privateNotifications';
