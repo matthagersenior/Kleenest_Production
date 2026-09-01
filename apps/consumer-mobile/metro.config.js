cd1f3a0c19ca6b9c0c574ab6c54a33e30fe91e10
matthagersenior <matthagersr@gmail.com>
2026-08-31 20:50:52 -0500

    Add web-only native module preview aliases

---
+++ apps/consumer-mobile/metro.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+const webAliases = {
+  '@maplibre/maplibre-react-native': path.resolve(__dirname, 'web/maplibrePreview.tsx'),
+  'expo-secure-store': path.resolve(__dirname, 'web/secureStorePreview.ts'),
+};
+
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (platform === 'web' && webAliases[moduleName]) {
+    return { type: 'sourceFile', filePath: webAliases[moduleName] };
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
+
+module.exports = config;
