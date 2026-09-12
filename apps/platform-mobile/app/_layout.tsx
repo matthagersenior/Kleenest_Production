a877683521c580d83575a405e5d5fae4f683fe37
matthagersenior <matthagersr@gmail.com>
2026-09-12 12:12:44 -0500

    feat: expose Developer Platform in KleenestOS

--- apps/platform-mobile/app/_layout.tsx
+++ apps/platform-mobile/app/_layout.tsx
@@ -33,6 +33,7 @@ export default function Layout(){
     <Tabs.Screen name="moderation" options={{title:'Moderation'}}/>
     <Tabs.Screen name="access" options={{title:'Access'}}/>
     <Tabs.Screen name="operations" options={{title:'Operations'}}/>
+    <Tabs.Screen name="developers" options={{title:'Developers'}}/>
     <Tabs.Screen name="pilots" options={{title:'Pilots'}}/>
     <Tabs.Screen name="auth" options={{href:null,title:'Sign in',headerShown:false}}/>
     <Tabs.Screen name="accounts" options={{href:null,title:'Accounts'}}/>
